c6a9509515aad785fc8e5a254e2f6af69bfe02df
shuhei kaneko <shihei-kaneko@tokubai.co.jp>
2023-08-16 13:27:10 +0900

    Initial commit

---
+++ lib/sample_gem_sk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sample_gem_sk/version"
+
+module SampleGemSk
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sample_gem_sk/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SampleGemSk
+  VERSION = "0.1.0"
+end
